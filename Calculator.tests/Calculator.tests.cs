1e8d7702beb7dbaac2db24d38eac74efe4086eac
Whit <whit@stroups.net>
2019-04-25 22:50:37 -0500

    added tests for multiply, divide and factorial

--- Calculator.tests/Calculator.tests.cs
+++ Calculator.tests/Calculator.tests.cs
@@ -22,7 +22,7 @@ namespace CalculatorWithTests.tests
         [Theory]
         [InlineData(100, 50, 50)]
         [InlineData(45.5, 65, -19.5)]
-        [InlineData(0, -1, -1)]
+        [InlineData(0, -1, 1)]
         public void ShouldSubtractTest(double subtractnumber1, double subtractnumber2, double expectednumber)
         {
             Calculator challenger = new Calculator();
@@ -32,6 +32,49 @@ namespace CalculatorWithTests.tests
             Assert.Equal(expectednumber, actual);
 
         }
+        [Theory]
+        [InlineData(100, 50, 5000)]
+        [InlineData(45.5, 65, 2957.5)]
+        [InlineData(0, -1, 0)]
+        public void ShouldMultipyTest(double multiplynumber1, double multiplynumber2, double expectednumber)
+        {
+            Calculator challenger = new Calculator();
+
+            double actual = challenger.multiply(multiplynumber1, multiplynumber2);
+
+            Assert.Equal(expectednumber, actual);
+
+        }
+        [Theory]
+        [InlineData(100, 50, 2)]
+        [InlineData(45.5, 65, .7)]
+        [InlineData(0, -1, 0)]
+        public void ShouldDivideTest(double dividenumber1, double dividenumber2, double expectednumber)
+        {
+            Calculator challenger = new Calculator();
+
+            double actual = challenger.divide(dividenumber1, dividenumber2);
+
+            Assert.Equal(expectednumber, actual);
+
+        }
+        [Theory]
+        [InlineData(10, 3628800)]
+        [InlineData(4, 24)]
+        [InlineData(0, 0)]
+        public void ShouldfactorialTest(double factor1, double expectednumber)
+        {
+            Calculator challenger = new Calculator();
+
+            double actual = challenger.factorial(factor1);
+
+            Assert.Equal(expectednumber, actual);
+
+        }
+
+
+
+
 
 
 
--- CalculatorWithTests/Calculator.cs
+++ CalculatorWithTests/Calculator.cs
@@ -6,6 +6,12 @@ namespace CalculatorWithTests
     public class Calculator
     {
         public double AdditionNumber(double number1, double number2)         {             return number1 + number2;         }
-        public double subtract(double subtractnumber1, double subtractnumber2)         {             return subtractnumber1 - subtractnumber2;         }          public double multiply(double multiplynumber1, double multiplynumber2)         {             throw new NotImplementedException();         }         public double divide(double dividenumber1, double dividenumber2)         {             throw new NotImplementedException();         }          public double factorial(double factor1)         {             throw new NotImplementedException();         } 
+        public double subtract(double subtractnumber1, double subtractnumber2)         {             return subtractnumber1 - subtractnumber2;         }          public double multiply(double multiplynumber1, double multiplynumber2)         {             return multiplynumber1 * multiplynumber2;         }         public double divide(double dividenumber1, double dividenumber2)         {             return dividenumber1 / dividenumber2;         }          public double factorial(double factor1)         {
+            double fact = factor1;             for (double i = factor1 - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+
+            return fact;         } 
     }
 }
